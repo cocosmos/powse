23053567c3aa19c2ea12cac3311f8ed2329e73a1
cocosmos <mipam.guillot@crea-inseec.com>
2022-05-16 18:19:04 +0200

    change landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,5 @@
-import { Box, Stack, Typography } from "@mui/material";
-import Logo from "../assets/Logo";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import Logo from "../assets/logo/logo.svg";
 import MockupHome from "../assets/mockup/mockup_home.webp";
 import MockupEvent from "../assets/mockup/mockup_event.webp";
 import SubmitButton from "../components/common/inputs/SubmitButton";
@@ -14,24 +14,45 @@ const Landing = () => (
     sx={{ margin: "0 auto" }}
   >
     {/*logo*/}
-    <Box component="span" mt={2}>
+    {/* <Box component="span" mt={2}>
       <Logo />
     </Box>
     <Typography variant="h1">La powse de votre vie !</Typography>
-
+ */}
     <Box
       flexGrow={1}
       display={"flex"}
-      mt={5}
+      /*    mt={5} */
       alignItems="center"
       width="100%"
       justifyContent={"center"}
       flexWrap="wrap"
     >
-      <img src={MockupHome} alt="Mockup home" style={{ maxWidth: "100%" }} />
-      <Box flexGrow={1}>
-        <Typography variant="h2">La powse de votre vie !</Typography>
-        <SubmitButton label={"S'inscrire"} type={"button"} href={"register"} />
+      <Box width={"50%"} textAlign="left">
+        <img src={Logo} />
+        <Typography variant="h1">
+          Consultez les différentes pauses de vos collègues.
+        </Typography>
+        <Typography variant="body1">
+          Rejoignez celles qui vous correspondent le plus !
+        </Typography>
+
+        <Button
+          variant="contained"
+          color="primary"
+          href={"register"}
+          sx={{
+            borderRadius: 25,
+            textTransform: "unset",
+            mt: 4,
+          }}
+          size="medium"
+        >
+          S'inscrire
+        </Button>
+      </Box>
+      <Box>
+        <img src={MockupHome} alt="Mockup home" style={{ maxWidth: "80%" }} />
       </Box>
     </Box>
 
